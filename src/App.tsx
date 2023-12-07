70e80c8f55ed1ea1ff01284ccd5fa7dd09b535f7
Eryk Dąbek <eryk.dabek00@gmail.com>
2023-12-07 19:41:20 +0100

    Hide app

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import { QueryClient, QueryClientProvider } from 'react-query'
-import { SearchBar, TaskList, TaskModal } from 'components'
+// import { SearchBar, TaskList, TaskModal } from 'components'
+import { TaskModal } from 'components'
 import { useToggle } from 'hooks'
 
 const queryClient = new QueryClient()
 
 function App() {
   const [isOpen, toggle] = useToggle()
-  const [searchName, setSearchName] = useState('')
+  // const [searchName, setSearchName] = useState('')
 
   return (
     <QueryClientProvider client={queryClient}>
       <TaskModal isOpen={isOpen} toggle={toggle} />
       <div className="m-auto mt-10 flex w-[650px] flex-col items-center justify-center space-y-3">
-        <h1 className="mb-5 text-6xl">Todo App</h1>
+        {/* <h1 className="mb-5 text-6xl">Todo App</h1>
         <SearchBar onAdd={toggle} setSearchName={setSearchName} />
-        <TaskList searchName={searchName} />
+        <TaskList searchName={searchName} /> */}
+        cześć
       </div>
     </QueryClientProvider>
   )
