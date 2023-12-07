8c885a330900102e60512a26ef8b1140d9cfb819
Eryk Dąbek <eryk.dabek00@gmail.com>
2023-12-07 20:15:17 +0100

    remove env variables

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,5 @@
-export const API_URL = (import.meta.env.VITE_API_URL || '') as string
-export const API_KEY = (import.meta.env.VITE_API_KEY || '') as string
+// export const API_URL = (import.meta.env.VITE_API_URL || '') as string
+// export const API_KEY = (import.meta.env.VITE_API_KEY || '') as string
+
+export const API_URL = 'https://localhost:8080'
+export const API_KEY = 'kopytko'
