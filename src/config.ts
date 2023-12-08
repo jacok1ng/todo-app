2b63c9c10255ef659887cb67f8b7b34efe24de30
Eryk Dąbek <eryk.dabek00@gmail.com>
2023-12-08 16:23:53 +0100

    Remove unnecessary yml file

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,2 @@
-// export const API_URL = (import.meta.env.VITE_API_URL || '') as string
-// export const API_KEY = (import.meta.env.VITE_API_KEY || '') as string
-
-export const API_URL = 'https://localhost:8080'
-export const API_KEY = 'kopytko'
+export const API_URL = (import.meta.env.VITE_API_URL || '') as string
+export const API_KEY = (import.meta.env.VITE_API_KEY || '') as string
